50d383c865fd67f43f2611c4d0c57725d9e2466c
Rafho <rafhoerdeft@gmail.com>
2023-06-23 23:27:17 +0700

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-npm init -y
-npm i express express-fileupload mysql2 sequelize cors
-npm i nodemon
--- config/connect.js
+++
@@ -1,8 +0,0 @@
-import {Sequelize} from "sequelize";
-
-const db = new Sequelize('node_crud','root','',{
-    host: 'localhost',
-    dialect: "mysql"
-});
-
-export default db;
